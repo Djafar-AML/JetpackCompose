06ac0f74c586003d005219d904fea1ef0531c454
jeff <jafaragha77@gmail.com>
2022-12-09 19:15:54 +0330

    - rememberSavable

--- app/src/main/java/com/example/jetpackcompose/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/home/HomeScreen.kt
@@ -5,7 +5,8 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -20,12 +21,25 @@ fun HomeScreen() {
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
     ) {
+
+        var message by rememberSaveable {
+            mutableStateOf("Hello compose!")
+        }
+
+        Text(text = message)
+
         Spacer(modifier = Modifier.height(32.dp))
-        Button(onClick = {showToast(context, "hi!")}) {
+
+        Button(onClick = { showToast(context, "hi!")
+            message = "hi!"
+        }) {
             Text(text = "Hi me!")
         }
+
+        Spacer(modifier = Modifier.height(42.dp))
 
     }
 }
@@ -39,12 +53,6 @@ fun showToast(_context: Context, toastText: String) {
 fun DefaultPreview() {
 
     JetpackComposeTheme {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            Arrangement.Center,
-            Alignment.CenterHorizontally,
-        ) {
-            HomeScreen()
-        }
+        HomeScreen()
     }
 }
