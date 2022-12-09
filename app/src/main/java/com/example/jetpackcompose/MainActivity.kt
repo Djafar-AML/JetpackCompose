281f886c8406f456c257c00ff7b982496c068178
jeff <jafaragha77@gmail.com>
2022-12-09 18:33:47 +0330

    - button and spacer

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,17 +3,6 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose.ui.home.HomeScreen
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
--- app/src/main/java/com/example/jetpackcompose/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/jetpackcompose/ui/home/HomeScreen.kt
@@ -1,32 +1,37 @@
 package com.example.jetpackcompose.ui.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 @Composable
 fun HomeScreen() {
 
+    val context = LocalContext.current
+
     Column(
         modifier = Modifier.fillMaxSize(),
-        Arrangement.Center,
-        Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Greeting("Android")
-        Greeting("Android")
-        Greeting("Android")
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(onClick = {showToast(context, "hi!")}) {
+            Text(text = "Hi me!")
+        }
+
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun showToast(_context: Context, toastText: String) {
+    Toast.makeText(_context, toastText, Toast.LENGTH_SHORT).show()
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -39,9 +44,7 @@ fun DefaultPreview() {
             Arrangement.Center,
             Alignment.CenterHorizontally,
         ) {
-            Greeting("Android")
-            Greeting("Android")
-            Greeting("Android")
+            HomeScreen()
         }
     }
 }
